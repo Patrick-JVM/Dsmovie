8cc47e6f3a68a6a58f85e1a79c635e4bd0906bbb
Patrick dos Santos Sousa <patrick.s.sousa@hotmail.com>
2022-03-13 09:07:25 -0300

    Save score

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>Cine Pipoca</h1>
+                    <h1>Popcorn Movies</h1>
                     <a href="https://github.com/Patrick-JVM">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
